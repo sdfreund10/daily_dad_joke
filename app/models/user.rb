a675ad24f3a7a15ad81f49b1f82ef2c643b40b6f
Stephen Freund <sdfreund10@gmail.com>
2018-06-30 11:28:28 -0400

    Lets do some linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates_format_of :phone_number, with: /\A\d{10}\Z/
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   validates_uniqueness_of :phone_number, scope: :email
   before_validation :reformat_phone_number
-  validates_inclusion_of %i(
+  validates_inclusion_of %i[
     monday tuesday wednesday thursday friday saturday sunday
-  ), in: [true, false]
+  ], in: [true, false]
   has_many :user_joke_histories, dependent: :delete_all
 
   def formatted_phone_number
--- app/services/message_sender.rb
+++ app/services/message_sender.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # frozeb_string_literal: true
 
 require 'net/http'
